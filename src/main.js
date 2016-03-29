a9f49b7f9e9642e4263ddc3bef2404aed285f468
kpie89 <kpie89@gmail.com>
2016-03-29 18:33:31 -0400

    initial commit for talk

---
+++ src/main.js
@@ -0,0 +1,66 @@
+'use strict';
+
+import Vue from 'vue';
+import App from './App.vue';
+
+
+
+
+new Vue({
+  el: '#demo',
+
+  data: {
+    tasks: [{
+      body: 'Learn Vue.js',
+      completed: false
+    }, {
+      body: 'Present your findings',
+      completed: false
+    }],
+
+    newTask: ''
+  },
+
+  filters: {
+    toBeDone: function(tasks) {
+      return tasks.filter(function(task) {
+        return !task.completed;
+      });
+    },
+
+    completedTask: function(tasks) {
+      return tasks.filter(function(task) {
+        return task.completed;
+      });
+    }
+  },
+
+  methods: {
+
+    addTask: function(e) {
+      e.preventDefault();
+      this.tasks.push({
+        body: this.newTask,
+        completed: false
+      });
+
+      this.newTask = '';
+    },
+
+    removeTask: function(task) {
+      this.tasks.$remove(task);
+    },
+
+    updateTask: function(task) {
+      this.removeTask(task);
+
+      this.newTask = task.body;
+
+      this.$$.editTask.focus();
+    },
+
+    taskDone: function(task) {
+      task.completed = true;
+    }
+  }
+});
